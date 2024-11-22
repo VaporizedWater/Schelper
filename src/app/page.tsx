7dacf182a90bff9eb96b19c56225e7b47b3b5ad4
Sammy <sammyanagolum@gmail.com>
2024-11-22 10:51:14 +0530

    Title alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Home = () => (
     <div className="bg-center bg-bottom bg-cover bg-[url('../../public/wcispsu.jpg')] h-screen max-w-screen justify-items-center">
-        <h1 className='text-white drop-shadow-xl pt-40 w-fit text-6xl text-center font-bold'>Class Scheduling App</h1>
+        <h1 className='text-white drop-shadow-xl pt-40 w-full text-6xl text-center font-bold'>Class Scheduling App</h1>
     </div>
 );
 
