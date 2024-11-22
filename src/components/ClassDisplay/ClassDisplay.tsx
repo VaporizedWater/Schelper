d00db9b57d82035b46538f361dfd17f0094eb7fa
Sammy <sammyanagolum@gmail.com>
2024-11-22 10:22:59 +0530

    temp

--- src/components/ClassDisplay/ClassDisplay.tsx
+++ src/components/ClassDisplay/ClassDisplay.tsx
@@ -28,13 +28,23 @@ const ClassDisplay = (props: ClassIdProps) => {
 
     return (!classData ? nullDisplay :
         <div className="w-fit p-4">
-            <ul className="flex flex-col border-4 border-gray">
-                <li key="1" className="border border-gray">{classData.catalog_num}</li>
-                <li key="2" className="border border-gray">{classData.class_num}</li>
-                <li key="3" className="border border-gray">{classData.course_num}</li>
-                <li key="4" className="border border-gray">{classData.course_subject}</li>
-                <li key="5" className="border border-gray">{classData.instructor_email}</li>
-                <li key="6" className="border border-gray">{classData.instructor_name}</li>
+            <div className="p-2 border-4 border-gray">
+                Class: {classData.course_subject}
+            </div>
+            <ul className="border-4 border-gray grid grid-cols-2">
+                <li key="1" className="border border-gray">Catalog Num: </li>
+                <li key="2" className="border border-gray">{classData.catalog_num}</li>
+                <li key="3" className="border border-gray">Class Num: </li>
+                <li key="4" className="border border-gray">{classData.class_num}</li>
+                <li key="5" className="border border-gray">Course Num </li>
+                <li key="6" className="border border-gray">{classData.course_num}</li>
+
+                <li key="7" className="border border-gray">Course Subject:</li>
+                <li key="8" className="border border-gray">{classData.course_subject}</li>
+                <li key="9" className="border border-gray">Instructor Email: </li>
+                <li key="10" className="border border-gray">{classData.instructor_email}</li>
+                <li key="11" className="border border-gray">Instructor Name:</li>
+                <li key="12" className="border border-gray">{classData.instructor_name}</li>
             </ul>
         </div>
     );
