f52168f911a6a2303535fe72b0d307e37942d3cc
Austinlei0318 <120297432+Austinlei0318@users.noreply.github.com>
2024-12-11 06:47:52 -0500

    Add a add tag button in Manage tag page

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,17 +1,37 @@
-
+'use client';
+import AddTagButton from "@/components/AddTagButton/AddTagButton";
 import TagDisplay from "@/components/TagDisplay/TagDisplay";
+import { useState } from "react";
 
 const ManageTags = () => {
+
+    const [tags, setTags] = useState([
+        { tagName: "Test", classes: [12, 13, 14, 15] },
+        { tagName: "Test", classes: [12, 13, 14, 15] },
+        { tagName: "Test", classes: [12, 13, 14, 15] },
+    ]);
+
+    // Add a new tag
+    const handleAddTag = (newTagName: string) => {
+        // Append a new tag with the entered name and an empty classes array
+        setTags((prevTags) => [...prevTags, { tagName: newTagName, classes: [9, 10, 11] }]);
+    };
+
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-right">
+
             <h1 className="text-4xl text-bold py-6 text-center">Manage Tags</h1>
+
+            <AddTagButton onAddTag={handleAddTag} />
+
+            {/* Display all tags*/}
             <div className="px-10 w-full flex flex-col gap-3">
-                <TagDisplay tagName="Test" classes={[12, 13, 14, 15]}></TagDisplay>
-                <TagDisplay tagName="Test" classes={[12, 13, 14, 15]}></TagDisplay>
-                <TagDisplay tagName="Test" classes={[12, 13, 14, 15]}></TagDisplay>
+                {tags.map((tag, index) => (
+                    <TagDisplay key={index} tagName={tag.tagName} classes={tag.classes} />
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default ManageTags;
