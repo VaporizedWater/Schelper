cefb3007b27f108e3bf0e265219308930308ae0f
VaporizedWater <92421695+VaporizedWater@users.noreply.github.com>
2025-01-04 12:21:47 -0500

    Wrapped FullCalendar in element for easy resizing and repositioning

--- src/components/Calendar2/Calendar2.tsx
+++ src/components/Calendar2/Calendar2.tsx
@@ -7,25 +7,26 @@ import { EventSourceInput } from "@fullcalendar/core/index.js";
 const Calendar2 = (props: CalendarProps) => {
     const events: EventSourceInput = [
         {
-          title: 'Class 1',
-          start: '2025-01-01T08:00:00',
-          end: '2025-01-01T09:00:00'
+            title: 'Class 1',
+            start: '2025-01-01T08:00:00',
+            end: '2025-01-01T09:00:00'
         },
-      ];
-      
+    ];
+
     return (
-        <FullCalendar
-            height={800}
-            plugins={[timeGridPlugin, interactionPlugin]}
-            editable
-            selectable
-            events={events}
-            slotDuration={'00:30:00'}
-            slotMinTime={'08:00:00'}
-            slotMaxTime={'17:00:00'}
-            expandRows
-            snapDuration={'00:05:00'}
-        />
+        <div className="w-1/2 self-center">
+            <FullCalendar
+                plugins={[timeGridPlugin, interactionPlugin]}
+                editable
+                selectable
+                events={events}
+                slotDuration={'00:30:00'}
+                slotMinTime={'08:00:00'}
+                slotMaxTime={'17:00:00'}
+                expandRows
+                snapDuration={'00:05:00'}
+            />
+        </div>
     );
 };
 
