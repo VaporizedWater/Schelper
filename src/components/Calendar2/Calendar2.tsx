b59b36590ace77fab7f7612a9967eb979bc84813
VaporizedWater <92421695+VaporizedWater@users.noreply.github.com>
2025-01-04 19:50:36 -0500

    Added multi-select, highlighting of selection, and clearing of selection with an example of how it can work

--- src/components/Calendar2/Calendar2.tsx
+++ src/components/Calendar2/Calendar2.tsx
@@ -2,7 +2,8 @@ import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { CalendarProps } from "@/lib/types";
-import { EventSourceInput } from "@fullcalendar/core/index.js";
+import { EventClickArg, EventSourceInput } from "@fullcalendar/core/index.js";
+import { useEffect, useRef } from "react";
 
 const Calendar2 = (props: CalendarProps) => {
     const events: EventSourceInput = [
@@ -11,20 +12,77 @@ const Calendar2 = (props: CalendarProps) => {
             start: '2025-01-01T08:00:00',
             end: '2025-01-01T09:00:00'
         },
+        {
+            title: 'Class 2',
+            start: '2025-01-02T09:00:00',
+            end: '2025-01-02T10:00:00'
+        },
     ];
+    let selectedEvents: HTMLElement[] = [];
+
+    const ctrlHeldRef = useRef(false);
+
+    function unselectAll() {
+        selectedEvents.forEach(element => {
+            if (element) {
+                element.style.borderColor = 'transparent';
+            }
+        });
+    
+        // Then, clear the array after the loop
+        selectedEvents.length = 0;  // This effectively empties the array
+    }
+
+    function downHandler(event: KeyboardEvent) {
+        if (event.key === 'Control') {
+            ctrlHeldRef.current = true;
+        }
+    }
+
+    function upHandler(event: KeyboardEvent) {
+        if (event.key === 'Control') {
+            ctrlHeldRef.current = false;
+            unselectAll();
+        }
+        console.log("Key up");
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', downHandler);
+        window.addEventListener('keyup', upHandler);
+
+        return () => {
+            window.removeEventListener('keydown', downHandler);
+            window.removeEventListener('keyup', upHandler);
+        };
+    }, [downHandler, upHandler]);
+
+    
 
     return (
-        <div className="w-1/2 self-center">
+        <div className="w-1/2 self-center select-none">
             <FullCalendar
                 plugins={[timeGridPlugin, interactionPlugin]}
                 editable
-                selectable
+                expandRows
+                selectable={false}
                 events={events}
                 slotDuration={'00:30:00'}
                 slotMinTime={'08:00:00'}
                 slotMaxTime={'17:00:00'}
-                expandRows
                 snapDuration={'00:05:00'}
+                eventClick={(info: EventClickArg) => {
+                    // Handle old elements
+                    if (!ctrlHeldRef) {
+                        unselectAll();
+                    }
+
+
+                    // Handle new element
+                    console.log(info.el.className);
+                    selectedEvents.push(info.el);
+                    info.el.style.borderColor = 'red';
+                }}
             />
         </div>
     );
