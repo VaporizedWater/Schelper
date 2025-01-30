a8adb5d4bdb338dc27a407bdcdc6398553d9bc9b
VaporizedWater <92421695+VaporizedWater@users.noreply.github.com>
2025-01-30 14:42:44 -0500

    archiving more unused files

---
+++ src/lib/archived/TimeGridCell/TimeGridCell.tsx
@@ -0,0 +1,19 @@
+// import { TimeGridCellProps } from "@/lib/types";
+// import { useDroppable } from "@dnd-kit/core";
+
+// const TimeGridCell = (props: TimeGridCellProps) => {
+//     const { isOver, setNodeRef } = useDroppable({
+//         id: props.droppableId,
+//     });
+//     const style = {
+//         color: isOver ? 'green' : undefined,
+//     };
+
+
+//     return (
+//         <div ref={setNodeRef} style={style} className="min-h-10 border-x border-b hover:bg-gray-100 duration-200">
+//         </div>
+//     );
+// }
+
+// export default TimeGridCell;
