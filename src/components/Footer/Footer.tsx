3b87aa29e757c5dd49049a67f49fe0fa68bdd9e1
Sammy <sammyanagolum@gmail.com>
2025-04-20 00:07:04 +0530

    temp

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,8 @@
-import { LogoIcon } from "@/lib/icons";
-
 const Footer = () => {
     return (
         <footer className="w-full bg-psublue p-5 text-white relative bottom-0 flex flex-row items-center gap-2">
             <span>Copyright © 2024 </span>
             <span className="flex flex-row items-center">
-                <LogoIcon height="36" width="36"/>
                 <p className="font-bold text-lg text-lightblue text-center p-1 ">Schelper</p>
             </span>
         </footer>
